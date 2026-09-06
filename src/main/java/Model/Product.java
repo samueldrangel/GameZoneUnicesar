bb84c07d7308e91e395b366bd4ebf5a1c502b233
Samuel Angulo Meza <sangulom@unicesar.edu.co>
2026-09-06 09:45:30 -0500

    feat: development of the product class

--- src/main/java/Model/Product.java
+++ src/main/java/Model/Product.java
@@ -8,6 +8,72 @@ package Model;
  *
  * @author Samuel
  */
-public class Product {
-    
+public abstract class Product {
+
+    private String id;
+    private String title;
+    private double price;
+    private int stock;
+
+    /**
+     * Default constructor.
+     */
+    public Product() {
+    }
+
+    /**
+     * Parameterized constructor to initialize a Product instance.
+     *
+     * @param id Unique identifier for the product
+     * @param title Title or name of the product
+     * @param price Unit price of the product
+     * @param stock Available inventory count
+     */
+    public Product(String id, String title, double price, int stock) {
+        this.id = id;
+        this.title = title;
+        this.price = price;
+        this.stock = stock;
+    }
+
+    // --- Getters and Setters ---
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    /**
+     * Abstract method to be implemented by concrete subclasses to provide
+     * specific product description details.
+     *
+     * @return Formatted description of the product
+     */
+    public abstract String getDescription();
 }
